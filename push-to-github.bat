73d8fd964723b2a98ddd3796587351c3608f2af1
greigorius <greigfensome@gmail.com>
2026-06-02 16:56:40 +0100

    Update 02/06/2026 16:56

--- push-to-github.bat
+++ push-to-github.bat
@@ -3,8 +3,10 @@ cd /d "%~dp0"
 git add -A
 git status
 echo.
-set /p msg="Commit message: "
+set "default=Update %date% %time:~0,5%"
+set /p msg="Commit message (Enter for default): "
+if "%msg%"=="" set "msg=%default%"
 git commit -m "%msg%"
-git push
+git push origin main
 echo.
 pause
